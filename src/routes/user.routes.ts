c331681059cad68d4e914e89564d39389e73228a
Diego <dl7designer@gmail.com>
2024-04-11 17:32:04 -0300

    alteracao na rota de usuario

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -5,7 +5,7 @@ import registerMiddleware from "../middlewares/register.middleware"
 export const userRoutes = () => {
     const router = Router()
     const controller = new UserController()
-    router.post('/', registerMiddleware, controller.create)
+    router.post('/create', registerMiddleware, controller.create)
 
     return router
 }
